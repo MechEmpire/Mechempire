e428b72b6a7042c1c6cb7f3f782b116931c38bc7
Tairy <tairyguo@gmail.com>
2015-04-12 21:11:38 +0800

    user login bug fix'

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,10 @@
 module SessionsHelper
   def sign_in(user)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
+    cookies[:remember_token] = {
+        :value => remember_token,
+        :httponly => true
+      }
     user.update_attribute(:remember_token, User.hash_custom(remember_token))
     self.current_user = user
   end
