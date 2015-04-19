9c5ff9ed4588cdbb9ab008362d883a9ab5923f7f
Tairy <tairyguo@gmail.com>
2015-04-19 10:38:58 +0800

    g++ bug

--- compile/compile_user_ai.sh
+++ compile/compile_user_ai.sh
@@ -4,7 +4,7 @@ while getopts "p:" arg
   do
     case $arg in
          p)
-            g++ -O2 -fPIC -shared -I/vagrant/Mechempire/compile/include/sys/ -I$OPTARG/ -o $OPTARG/libmyAI.so $OPTARG/*.cpp /vagrant/Mechempire/compile/include/sys/*.cpp /vagrant/Mechempire/compile/include/RobotAIFactoryLinux.cpp
+            /usr/bin/g++ -O2 -fPIC -shared -I/vagrant/Mechempire/compile/include/sys/ -I$OPTARG/ -o $OPTARG/libmyAI.so $OPTARG/*.cpp /vagrant/Mechempire/compile/include/sys/*.cpp /vagrant/Mechempire/compile/include/RobotAIFactoryLinux.cpp
             ;;
          ?)
         echo "unkonw argument"
