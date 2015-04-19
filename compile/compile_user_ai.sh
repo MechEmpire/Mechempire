53a42a30eb6b4520f721b79016380117a3f75b52
Tairy <tairyguo@gmail.com>
2015-04-19 19:01:30 +0800

    battle show

--- compile/compile_user_ai.sh
+++ compile/compile_user_ai.sh
@@ -4,11 +4,11 @@ while getopts "p:" arg
   do
     case $arg in
          p)
-            /usr/bin/g++ -O2 -fPIC -shared -I/vagrant/Mechempire/compile/include/sys/ -I$OPTARG/ -o $OPTARG/libmyAI.so $OPTARG/*.cpp /vagrant/Mechempire/compile/include/sys/*.cpp /vagrant/Mechempire/compile/include/RobotAIFactoryLinux.cpp
+            g++ -O2 -fPIC -shared -I/vagrant/Mechempire/compile/include/sys/ -I$OPTARG/ -o $OPTARG/libmyAI.so $OPTARG/*.cpp /vagrant/Mechempire/compile/include/sys/*.cpp /vagrant/Mechempire/compile/include/RobotAIFactoryLinux.cpp
             ;;
          ?)
         echo "unkonw argument"
     exit 1
-    ;;
+    ;;  
   esac
 done
