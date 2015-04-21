4cf7ced4f10326e263e5bda9e09e55f4eba0c991
Tairy <tairyguo@gmail.com>
2015-04-21 21:14:52 +0800

    add open1

--- battle/battle.sh
+++ battle/battle.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+PATH=$PATH:"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 (ulimt -t 60 -m 131072;
 timeout 60 battle/MechBattleConsoleForLinuxServer battle/BattleModeConfig.conf 2 $1 $2 battle/result/$3.txt battle/result/$3.xml)
