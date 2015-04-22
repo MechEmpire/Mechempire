0c8f33cf40f1c0ddd28379987950a198c00d31ce
thermal <xieshufang76@gmail.com>
2015-04-22 21:44:10 +0800

    remove setuid

--- battle/run.sh
+++ battle/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ulimit -t 60 -m 131072;
-setuid 2333 timeout 60 /MechBattleConsoleForLinuxServer /BattleModeConfig.conf 2 /1.so /2.so /$3.txt /$3.xml
+timeout 60 /MechBattleConsoleForLinuxServer /BattleModeConfig.conf 2 /1.so /2.so /$3.txt /$3.xml
