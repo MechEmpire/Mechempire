39cbc83f1b7584de83c8c40a40b834024109e27d
thermal <xieshufang76@gmail.com>
2015-04-22 18:52:44 +0800

    seperate

--- battle/result.sh
+++ battle/result.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PATH=$PATH:"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-cp /tmp/jail/$3.txt /home/rails-deploy/Mechempire/battle/result/$3.txt
-cp /tmp/jail/$3.xml /home/rails-deploy/Mechempire/battle/result/$3.xml
+JAIL_PATH = /tmp/jail_$3
+cp JAIL_PATH/$3.txt /home/rails-deploy/Mechempire/battle/result/$3.txt
+cp JAIL_PATH/$3.xml /home/rails-deploy/Mechempire/battle/result/$3.xml
