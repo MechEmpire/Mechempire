1e066adea2f1e577cd440fda51f37770259f721c
thermal <xieshufang76@gmail.com>
2015-04-22 18:34:09 +0800

    battle

--- battle/result.sh
+++ battle/result.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+PATH=$PATH:"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 cp /tmp/jail/$3.txt /home/rails-deploy/Mechempire/battle/result/$3.txt
 cp /tmp/jail/$3.xml /home/rails-deploy/Mechempire/battle/result/$3.xml
