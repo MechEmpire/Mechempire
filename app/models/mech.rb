feb72a987322a5567d2dea19c448e9af0c71ec39
Tairy <tairyguo@gmail.com>
2015-04-24 19:43:33 +0800

    fix bug

--- app/models/mech.rb
+++ app/models/mech.rb
@@ -78,7 +78,7 @@ class Mech
     pid, stdin, stdout, stderr = Open4.popen4("compile/compile_user_ai.sh -p #{self.code_dir}")
     ignored, status = Process::waitpid2 pid
 
-    compile_error = stderr.read.strip.gsub(self.code_dir, "")
+    compile_error = stderr.read
 
     File.open("public/uploads/#{self.class.to_s.underscore}/code/#{self.id}/code/status.txt", "w+") do |f|
       f.write(status.exitstatus)
